3a6103c7101771aaa405c4da8176458b233b47e5
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-11-14 10:49:30 +0700

    add json2.js

--- js/nm.js
+++ js/nm.js
@@ -1,6 +1,6 @@
 var Nm = {};
 
-Nm.createXHR = function(url, options) {
+Nm.createXHR = function( url, options ) {
 	var xhr = false;
 
 	if ( window.XMLHttpRequest ) {
@@ -13,30 +13,30 @@ Nm.createXHR = function(url, options) {
 
 		xhr.onreadystatechange = function() {
 			if ( ( xhr.readyState === 4 ) && ( xhr.status === 200 || xhr.status === 304 ) ) {
-				var contentType = xhr.getResponseHeader('Content-Type');
+				var contentType = xhr.getResponseHeader( 'Content-Type' );
 				if ( options.complete ) {
-					if ( contentType.search(/json/) > 1 ) {
-						options.complete.call(xhr, JSON.parse(xhr.responseText));
-					} else if ( contentType.search(/xml/) > 1 ) {
-						options.complete.call(xhr, xhr.responseXML);
+					if ( contentType.search( /json/ ) > 1 ) {
+						options.complete.call( xhr, JSON.parse( xhr.responseText ) );
+					} else if ( contentType.search( /xml/ ) > 1 ) {
+						options.complete.call( xhr, xhr.responseXML );
 					} else {
-						options.complete.call(xhr, xhr.responseText);
+						options.complete.call( xhr, xhr.responseText );
 					}
 				}
 			}
 		};
 
-		xhr.open(options.method, url, true);
+		xhr.open( options.method, url, true );
 		return xhr;
 	} else {
 		return false;
 	}
 };
 
-Nm.ajax = function(url, options) {
-	var xhr = Nm.createXHR(url, options);
+Nm.ajax = function( url, options ) {
+	var xhr = Nm.createXHR( url, options );
 
 	if ( xhr ) {
-		xhr.send(null);
+		xhr.send( null );
 	}
 };
